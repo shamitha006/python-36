2e255d5f54a7e381d17570485184091e75f53ebe
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2021-08-28 23:52:21 +0530

    Update Activity_09.py

---
+++ Activity_09.py
@@ -0,0 +1,9 @@
+import math
+l=float(input("Enter the length"))
+b=float(input("Enter the breadth"))
+h=float(input("Enter the height"))
+k=(l**2)+(b**2)+(h**2)
+vol=((b**2)*(h**2))/(k**(1/2))
+print(vol)
+r=((3*vol)/(4*math.pi))**(1/3)
+print(r)
