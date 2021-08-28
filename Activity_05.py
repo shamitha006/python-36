5d6810e3cad6b7ef31160ed6e6a1019264b8cd79
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2021-08-28 18:40:08 +0530

    Update Activity_05.py

--- Activity_05.py
+++ Activity_05.py
@@ -1,8 +1,10 @@
 s=input("Enter 5 numbers separated by spaces")
 nos=list()
+num=list()
 Sum=0
 nos=s.split(" ")
 for i in nos:
-    i=int(i)
-Sum=sum(nos)
+    j=int(i)
+    num.append(j)
+Sum=sum(num,0)
 print("Sum of all the numbers is =",Sum)
