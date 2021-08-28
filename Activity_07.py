202f1e7210e863b8dbea182e744e4c72a0c1bed1
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2021-08-28 23:37:31 +0530

    Update Activity_07.py

--- Activity_07.py
+++ Activity_07.py
@@ -1,7 +1,7 @@
 num=input("Enter two numbers to be added separated by a space")
 nos=num.split()
 Sum=0
-res=map(lambda x:int(x),nos)
+res=list(map(lambda x:int(x),nos))
 add=list(res)
 Sum=sum(add,0)
 print("%d + %d = %d"%(add[0],add[1],Sum))
