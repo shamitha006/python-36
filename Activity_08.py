00cad07231ac0cc26699a7e6d096d610260633eb
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2021-08-29 00:23:51 +0530

    Update Activity_08.py

--- Activity_08.py
+++ Activity_08.py
@@ -1,3 +1,3 @@
-An interpreted language executes the program directly without really converting it into any machine understandable language.
+An interpreted language executes the program directly without before converting it into any machine understandable language.
 Interpreter converts and checks one line of the code at a time whereas a compiler converts a whole program and checks.
 It is written using C and called CPython
