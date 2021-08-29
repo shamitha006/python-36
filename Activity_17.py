b331af9851ffb87a1967f09af9d02f2cdb9cfbd9
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2021-08-29 19:16:48 +0530

    Update Activity_17.py

--- Activity_17.py
+++ Activity_17.py
@@ -1,10 +1,21 @@
-string=input("Enter the string").split(";")
-res=list()
-for i in string:
-    t=list()
-    new=i.split("=")
-    t.append(new[0])
-    t.append(new[1])
-    tuple(t)
-    res.append(t)
-print(res)
+def getstr():
+    string=input("Enter the string").split(";")
+    return string
+def convert(string):
+    res=list()
+    for i in string:
+        t=list()
+        new=i.split("=")
+        t.append(new[0])
+        t.append(new[1])
+        tuple(t)
+        res.append(t)
+    return res
+def output(res):
+    print(res)
+def main():
+    string=getstr()
+    res=convert(string)
+    output(res)
+main()
+
