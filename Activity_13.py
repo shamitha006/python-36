b80e3ddfed99eb483fa901be17d0c2218df937b0
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2021-08-29 11:56:29 +0530

    Update Activity_13.py

--- Activity_13.py
+++ Activity_13.py
@@ -2,6 +2,6 @@ x=int(input("Enter the start number:"))
 y=int(input("Enter the stop number:"))
 for i in range(x,y):
     if(i%2!=0):
-        print(i,end=",")
+        print(i,end=", ")
     else:
         continue
