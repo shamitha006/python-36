43ed940491eb4bee4db8b6701ff22ce7e2e2b45b
Shamitha T <72788008+shamitha006@users.noreply.github.com>
2021-08-29 12:01:59 +0530

    Update Activity_13.py

--- Activity_13.py
+++ Activity_13.py
@@ -1,7 +1,14 @@
-x=int(input("Enter the start number:"))
-y=int(input("Enter the stop number:"))
-for i in range(x,y):
-    if(i%2!=0):
-        print(i,end=", ")
-    else:
-        continue
+def input_nos():
+    x=int(input("Enter the start number:"))
+    y=int(input("Enter the stop number:"))
+    return x,y
+def check(x,y):
+    for i in range(x,y):
+        if(i%2!=0):
+            print(i,end=", ")
+        else:
+            continue
+def main():
+    a,b=input_nos()
+    check(a,b)
+main()
